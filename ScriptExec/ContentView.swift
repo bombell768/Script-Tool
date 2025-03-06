825dfc30b6efff0f9fec8a057954ef6e14d592c6
bombell768 <266878@student.pwr.edu.pl>
2025-03-06 21:32:52 +0100

    Initial Commit

---
+++ ScriptExec/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScriptExec
+//
+//  Created by Bartosz Strzecha on 06/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScriptExec/ScriptExecApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScriptExecApp.swift
+//  ScriptExec
+//
+//  Created by Bartosz Strzecha on 06/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScriptExecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
